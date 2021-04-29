4e769de827d9e5ce7776e7ec46d96e9932fa37df
Thomas <substantialimpulse@pm.me>
2021-04-29 14:52:28 +0200

    add custom settings to nuxt/eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,22 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:vue/strongly-recommended',
+    'plugin:nuxt/recommended',
+    'standard'
   ],
-  plugins: [
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'multiline-ternary': 'warn',
+    eqeqeq: 'warn', // don't error on wrong equality signs
+    camelcase: 'warn', // don't error on non camel case
+    quotes: 'warn', // don't error on double qoutes
+    'comma-dangle': 'warn',
+    'spaced-comment': 'warn',
+    semi: 'off', // no preference for semicolons at end
+    'no-warning-comments': 'warn',
+    'vue/valid-v-slot': 'off', // false positives for vuetify
+    'vue/html-quotes': 'off' // doesn't work properly
+  }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - reims2-frontend',
-    title: 'reims2-frontend',
+    titleTemplate: '%s - Reims2',
+    title: 'Reims2',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -23,6 +23,19 @@ export default {
   plugins: [
   ],
 
+  ssr: false, // Disable Server Side rendering
+
+  /*
+  ** Customize the automatic progress-bar
+  ** https://nuxtjs.org/docs/2.x/features/loading
+  */
+  loading: {
+    color: '#1976D2', // blue.darken2
+    height: '3px',
+    duration: 1000,
+    continuous: true
+  },
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -31,7 +44,9 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    // https://google-fonts.nuxtjs.org
+    '@nuxtjs/google-fonts'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -52,22 +67,26 @@ export default {
     }
   },
 
+  // https://google-fonts.nuxtjs.org/options
+  googleFonts: {
+    download: true,
+    families: {
+      Roboto: true
+    }
+  },
+
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      light: true
+    },
+    defaultAssets: false, // dont use CDNs for libraries
+    breakpoint: {
+      mobileBreakpoint: 'sm' // This is equivalent to a value of 960
+    },
+    icons: {
+      iconfont: 'mdiSvg'
     }
   },
 
