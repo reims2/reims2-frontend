0fe7ac071e2aab8e1705d205dad3b64fb1b4653c
Thomas <substantialimpulse@pm.me>
2021-04-30 16:13:55 +0200

    add a default page

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,31 @@
         <v-card-title class="headline">
           Welcome to REIMS2
         </v-card-title>
-        <v-card-text class="text-center">
-          <v-btn
-            v-for="item in items"
-            :key="item.to"
-            :to="item.to"
-            nuxt
-            class="ma-2"
-          >
-            <v-icon>{{ item.icon }}</v-icon>
-            {{ item.text }}
-          </v-btn>
+        <v-card-text>
+          <v-container class="pt-10">
+            <v-row>
+              <v-col cols=12>
+                <v-alert
+                  border="left"
+                  colored-border
+                  type="warning"
+                  elevation="2"
+                >
+                  In its current state the UI has no real functions. The displayed data is the example data from 1998-2000.
+                </v-alert>
+              </v-col>
+              <v-col class="d-flex justify-center">
+                <v-btn
+                  to="/find"
+                  nuxt
+                  color="primary"
+                  rounded
+                >
+                  Open current version of REIMS
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card-text>
       </v-card>
     </v-col>
@@ -23,29 +37,7 @@
 </template>
 
 <script>
-import { mdiFileFindOutline, mdiPlusBox, mdiDatabaseOutline } from '@mdi/js';
 export default {
-  layout: 'start',
-  data () {
-    return {
-      items: [
-        {
-          text: 'Find glasses',
-          to: 'find',
-          icon: mdiFileFindOutline
-        },
-        {
-          text: 'Add glasses',
-          to: 'add',
-          icon: mdiPlusBox
-        },
-        {
-          text: 'View data',
-          to: 'data',
-          icon: mdiDatabaseOutline
-        }
-      ]
-    }
-  }
+  layout: 'start'
 }
 </script>
