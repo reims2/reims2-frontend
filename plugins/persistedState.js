84775a2aa18b8ada2da9e5b4c3cd054539543545
Thomas <substantialimpulse@pm.me>
2021-05-07 22:56:48 +0200

    add samesite cookie attribute

--- plugins/persistedState.js
+++ plugins/persistedState.js
@@ -7,7 +7,7 @@ export default ({ store }) => {
     storage: {
       getItem: key => Cookies.get(key),
       // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
-      setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: false }),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 3, sameSite: 'strict', secure: false }),
       removeItem: key => Cookies.remove(key)
     }
   })(store)
