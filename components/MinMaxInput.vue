cd10e85e7814d223d2a060b2ff18328cf6cc5ae0
Thomas <substantialimpulse@pm.me>
2021-05-07 20:02:45 +0200

    add filtering

---
+++ components/MinMaxInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex align-end fill-height mb-2">
+    <v-text-field
+      v-model="min"
+      type="number"
+      step="0.25"
+      label="Min"
+      hide-details
+      dense
+      style="max-width:60px"
+      class="mr-1"
+      @change="update"
+    />
+    <v-text-field
+      v-model="max"
+      type="number"
+      step="0.25"
+      hide-details
+      dense
+      style="max-width:60px"
+      label="Max"
+      @change="update"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    min: '',
+    max: ''
+  }),
+  methods: {
+    update() {
+      this.$emit('update', { min: this.min, max: this.max })
+    }
+  }
+}
+</script>
--- pages/manage/data.vue
+++ pages/manage/data.vue
@@ -1,14 +1,11 @@
 <template>
-  <v-container class="px-2 px-md-10" fluid>
+  <v-container class="px-1 px-lg-10" fluid>
     <v-row dense class="d-flex justify-center">
-      <v-col class="text-body-2 text--secondary pb-6">
-        You can sort this table by clicking on the column names. Further features will follow.
-      </v-col>
       <v-col cols=12>
         <v-data-table
           fixed-header
           :headers="headers"
-          :items="glasses"
+          :items="items"
           :items-per-page="20"
           dense
           must-sort
@@ -18,7 +15,40 @@
             itemsPerPageOptions: [10,20,100,500],
             showCurrentPage: true
           }"
-        />
+        >
+          <template v-if="!$vuetify.breakpoint.mobile" #body.prepend>
+            <tr>
+              <td />
+              <td>
+                <div class="d-flex mt-3 mb-1">
+                  <v-select
+                    multiple
+                    dense
+                    hide-details
+                    label="Type"
+                    :items="['single', 'bifocal', 'progressive']"
+                    style="max-width:250px;"
+                    @change="value => {filterType = value}"
+                  />
+                </div>
+              </td>
+              <td>
+                <min-max-input @update="value => {updateFilter(value, 'od', 'sphere')}" />
+              </td>
+              <td>
+                <min-max-input @update="value => {updateFilter(value, 'od', 'cyl')}" />
+              </td>
+              <td />
+              <td />
+              <td>
+                <min-max-input @update="value => {updateFilter(value, 'os', 'sphere')}" />
+              </td>
+              <td>
+                <min-max-input @update="value => {updateFilter(value, 'os', 'cyl')}" />
+              </td>
+            </tr>
+          </template>
+        </v-data-table>
       </v-col>
     </v-row>
   </v-container>
@@ -29,25 +59,48 @@ import { mapState, mapActions } from 'vuex'
 export default {
   title: 'View and analyse data',
   data: () => ({
-    headers: [
-      { value: 'sku', text: 'SKU' },
-      { value: 'type', text: 'TYPE' },
-      { value: 'odsphere', text: 'OD SPHERE' },
-      { value: 'odcylinder', text: 'OD CYLINDER' },
-      { value: 'odaxis', text: 'OD AXIS' },
-      { value: 'odadd', text: 'OD ADD' },
-      { value: 'ossphere', text: 'OS SPHERE' },
-      { value: 'oscylinder', text: 'OS CYLINDER' },
-      { value: 'osaxis', text: 'OS AXIS' },
-      { value: 'osadd', text: 'OS ADD' },
-      { value: 'appearance', text: 'APPEARANCE' },
-      { value: 'size', text: 'SIZE' }
-    ]
+    filterType: [],
+    filters: {
+      od: {
+        sphere: {},
+        cyl: {}
+      },
+      os: {
+        sphere: {},
+        cyl: {}
+      }
+    }
   }),
   computed: {
     ...mapState({
       glasses: state => state.glasses.glasses
-    })
+    }),
+    headers() {
+      return [
+        { value: 'sku', text: 'SKU' },
+        { value: 'type', text: 'TYPE' },
+        { value: 'odsphere', text: 'OD SPHERE' },
+        { value: 'odcylinder', text: 'OD CYLINDER' },
+        { value: 'odaxis', text: 'OD AXIS' },
+        { value: 'odadd', text: 'OD ADD' },
+        { value: 'ossphere', text: 'OS SPHERE' },
+        { value: 'oscylinder', text: 'OS CYLINDER' },
+        { value: 'osaxis', text: 'OS AXIS' },
+        { value: 'osadd', text: 'OS ADD' },
+        { value: 'appearance', text: 'APPEARANCE' },
+        { value: 'size', text: 'SIZE' }
+      ]
+    },
+    items() {
+      return this.glasses.filter((el) => {
+        if (this.filterType.length > 0 && !this.filterType.includes(el.type)) return false
+        if (!this.isInLimits(el.odsphere, this.filters.od.sphere)) return false
+        if (!this.isInLimits(el.odcylinder, this.filters.od.cyl)) return false
+        if (!this.isInLimits(el.ossphere, this.filters.os.sphere)) return false
+        if (!this.isInLimits(el.oscylinder, this.filters.os.cyl)) return false
+        return true
+      })
+    }
   },
   mounted() {
     this.load()
@@ -55,7 +108,18 @@ export default {
   methods: {
     ...mapActions({
       load: 'glasses/loadActiveGlasses'
-    })
+    }),
+    isInLimits(value, filters) {
+      const min = filters.min !== '' && filters.min !== undefined ? filters.min : null
+      const max = filters.min !== '' && filters.min !== undefined ? filters.min : null
+      if (min != null && max != null) return value >= min && value <= max
+      else if (min != null) return value >= min
+      else if (max != null) return value <= max
+      else return true
+    },
+    updateFilter(value, eye, child) {
+      this.filters[eye][child] = value
+    }
   }
 }
 </script>
