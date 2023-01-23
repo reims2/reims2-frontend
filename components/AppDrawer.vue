5b63d31c4483e607fcbf7360fa0a3deb89c81326
Thomas <substantialimpulse@pm.me>
2023-01-23 13:05:08 +0100

    Add a dev/staging indicator

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -24,47 +24,66 @@
     <v-divider v-if="!miniDrawer" class="mt-3" />
 
     <v-list v-if="!$vuetify.breakpoint.mobile" nav>
-      <v-list-item
-        v-for="item in mainItems"
-        :key="item.title"
-        :to="item.to"
-        :disabled="item.disabled || false"
-        nuxt
-        active-class="highlighted"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
+      <v-list-item-group color="accent">
+        <v-list-item
+          v-for="item in mainItems"
+          :key="item.title"
+          :to="item.to"
+          :disabled="item.disabled || false"
+          nuxt
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
     </v-list>
     <v-divider v-if="!$vuetify.breakpoint.mobile" class="mb-3" />
 
     <v-list nav subheader>
-      <v-list-item
-        v-for="item in otherItems"
-        :key="item.title"
-        :to="item.to"
-        :disabled="item.disabled || false"
-        nuxt
-        active-class="highlighted"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
+      <v-list-item-group color="accent">
+        <v-list-item
+          v-for="item in otherItems"
+          :key="item.title"
+          :to="item.to"
+          :disabled="item.disabled || false"
+          nuxt
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
     </v-list>
 
     <template #append>
       <v-divider />
       <v-list dense nav>
+        <v-list-item
+          v-if="isDev"
+          :href="commitUrl"
+          tabindex="-1"
+          target=”_blank”
+          rel=noopener
+          class="dev-background white--text"
+        >
+          <v-list-item-icon>
+            <v-icon color="white">
+              {{ mdiBug }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>!! DEV INSTANCE !!</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item @click.stop="dialog=true">
           <v-list-item-icon>
             <v-icon>{{ mdiMapMarkerMultiple }}</v-icon>
@@ -88,7 +107,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import { mdiLogout, mdiMapMarkerMultiple } from '@mdi/js'
+import { mdiLogout, mdiMapMarkerMultiple, mdiBug } from '@mdi/js'
 import { locationNames } from '../lib/util'
 export default {
   props: {
@@ -104,9 +123,12 @@ export default {
   data() {
     return {
       mdiLogout,
+      mdiBug,
       locationNames,
       mdiMapMarkerMultiple,
-      dialog: false
+      dialog: false,
+      isDev: process.env.isDev,
+      version: process.env.version
     }
   },
   computed: {
@@ -118,13 +140,16 @@ export default {
       get() { return !this.$vuetify.breakpoint.mobile && !this.drawer },
       set() { }
     },
+    commitUrl: function() {
+      return 'https://github.com/reims2/reims2-frontend/commit/' + this.version
+    },
     ...mapState(['drawer', 'location'])
   }
 }
 </script>
 
 <style scoped>
-.highlighted {
-  color: var(--v-accent-base) !important
+.dev-background {
+  background: var(--v-accent-darken1) !important
 }
 </style>
