ff91f48562fa33fa5c1181885aae026f2cfb432d
Thomas <substantialimpulse@pm.me>
2023-07-05 21:29:47 +0200

    readd stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,25 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript"
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
-  },
-}
+    "vue/multi-word-component-names": "off",
+    "multiline-ternary": "warn",
+    eqeqeq: "warn", // don't error on wrong equality signs
+    camelcase: "warn", // don't error on non camel case
+    quotes: "warn", // don't error on double qoutes
+    "comma-dangle": "warn",
+    "spaced-comment": "warn",
+    semi: "warn", // no preference for semicolons at end
+    "no-warning-comments": "warn",
+    "space-before-function-paren": ["warn", "never"], // no space before function parentheses because that looks weird
+    "no-console": "off",
+    "@typescript-eslint/no-var-requires": "warn"
+  }
+};
--- README.md
+++ README.md
@@ -1,44 +1,16 @@
-# default
+# REIMS2 frontend
 
-## Project setup
+![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/reims2/reims2-frontend/ci.yml?branch=main&label=ci)
+![Uptime](https://img.shields.io/uptimerobot/ratio/m789007197-530f6cec68b3f8f49de17c99?label=uptime)
 
+This is the frontend for [REIMS2](https://reims2.app)
+
+## Build Setup
+
+```bash
+# Install dependencies
+yarn install
+
+# Serve with hot reload at http://localhost:3000
+API_URL=https://dev.reims2.app yarn run dev
 ```
-# yarn
-yarn
-
-# npm
-npm install
-
-# pnpm
-pnpm install
-```
-
-### Compiles and hot-reloads for development
-
-```
-# yarn
-yarn dev
-
-# npm
-npm run dev
-
-# pnpm
-pnpm dev
-```
-
-### Compiles and minifies for production
-
-```
-# yarn
-yarn build
-
-# npm
-npm run build
-
-# pnpm
-pnpm build
-```
-
-### Customize configuration
-
-See [Configuration Reference](https://vitejs.dev/config/).
