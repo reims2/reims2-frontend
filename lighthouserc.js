59c827e86b16a9831fed23ecf4ff30b67cd99af1
Thomas <substantialimpulse@pm.me>
2023-07-20 14:23:22 +0200

    fix lighthouse conf

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,18 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './dist',
+      url: [
+        'http://localhost/',
+        'http://localhost/login',
+        'http://localhost/add',
+        'http://localhost/edit',
+        'http://localhost/view',
+        'http://localhost/find',
+        'http://localhost/manage/users',
+        'http://localhost/manage/reports',
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
