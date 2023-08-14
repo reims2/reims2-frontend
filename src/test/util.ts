192e67cd1a440463f40ba86dd1b14d6631086f78
Thomas <substantialimpulse@pm.me>
2023-08-14 20:05:11 +0200

    prepare support for store mocking

--- src/test/util.ts
+++ src/test/util.ts
@@ -1,13 +1,19 @@
 import { render, RenderOptions, RenderResult } from '@testing-library/vue'
 import { createVuetify } from 'vuetify'
+import { createTestingPinia, TestingPinia } from '@pinia/testing'
+import { vuetifyOptions } from '@/plugins/vuetify'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function renderWithVuetify(component: any, options: RenderOptions) {
-  const vuetify = createVuetify()
+export function renderWithVuetify(
+  component: any,
+  options: RenderOptions,
+  pinia?: TestingPinia | undefined,
+) {
+  const vuetify = createVuetify(vuetifyOptions)
   return render(component, {
     ...options,
     global: {
-      plugins: [vuetify],
+      plugins: [vuetify, pinia || createTestingPinia()],
     },
   })
 }
