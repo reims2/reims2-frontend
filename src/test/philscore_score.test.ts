821ac8d7fa4988695a0f48c288a8c7e8448aa08e
Thomas <substantialimpulse@pm.me>
2023-10-05 19:44:49 +0200

    refactor lens filtering

--- src/test/philscore_score.test.ts
+++ src/test/philscore_score.test.ts
@@ -23,6 +23,22 @@ test('Equal glasses and search returns philscore 0', () => {
   expect(result[0].score).toBe(0)
 })
 
+test('sphericalEquivalentScore applied', () => {
+  const glasses = createGlassesEqualOdOs('single', {
+    sphere: -0.25,
+    cylinder: 0,
+    axis: 180,
+  })
+  const search = createSearchOdOnly('single', {
+    sphere: 0.25,
+    cylinder: -1,
+    axis: 180,
+  })
+  const result = calculateAllPhilscore(search, glasses)
+  expect(result).toHaveLength(1)
+  expect(result[0].score).toBeCloseTo(1.25, 3)
+})
+
 test('REIMS1: sphericalEquivalentScore applied combined with smallerLensSphereScore. Also sphericalEquivalentScore as well as contraryDiffsScore could apply.', () => {
   const glasses = createGlassesEqualOdOs('single', {
     sphere: 0.25,
@@ -73,18 +89,18 @@ test('contraryDiffsScore applied with big cylinder diff and equal diffs', () => 
 
 test('contraryDiffsScore applied with small cylinder diff', () => {
   const glasses = createGlassesEqualOdOs('single', {
-    sphere: 0.75,
-    cylinder: -0.75,
-    axis: 180,
-  })
-  const search = createSearchOdOnly('single', {
     sphere: 0.25,
     cylinder: -0.5,
     axis: 180,
   })
+  const search = createSearchOdOnly('single', {
+    sphere: 0.75,
+    cylinder: -0.75,
+    axis: 180,
+  })
   const result = calculateAllPhilscore(search, glasses)
   expect(result).toHaveLength(1)
-  expect(result[0].score).toBeCloseTo(0.5, 3)
+  expect(result[0].score).toBeCloseTo(0.75, 3)
 })
 
 test('REIMS1: contraryDiffsScore applied with small cylinder diff and equal diffs', () => {
