4a90236908d82656b0fecaf07013cf5613bb0765
l3d00m <github@kxlion.de>
2024-01-23 09:28:22 +0100

    fix(table): items per page had wrong values

--- src/views/ViewGlasses.vue
+++ src/views/ViewGlasses.vue
@@ -93,10 +93,10 @@ const headers = [
 
 const itemsPerPageOptions = [
   { value: 10, title: '10' },
-  { value: 25, title: '20' },
+  { value: 20, title: '20' },
   { value: 50, title: '50' },
   { value: 100, title: '100' },
-  { value: 500, title: '250' },
+  { value: 250, title: '250' },
 ]
 
 const page = ref(1)
